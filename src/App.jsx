a0032ecef1c45bba3c1b600c74ad9876a25c319b
Vigneshwaran Sivalingam <vigneshsika@gmail.com>
2026-07-16 14:15:05 +0530

    Add header section

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <>
-     
-    </>
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+    </main>
   )
 }
 
